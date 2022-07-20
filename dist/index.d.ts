e3ef01231fa14eb61a916154f5b7d4cd7bbd1de6
lim-kim930 <1625753207lim@gmail.com>
2022-07-20 21:08:04 +0800

    update npm

---
+++ dist/index.d.ts
@@ -0,0 +1,34 @@
+/**
+ * MIT License
+ * Copyright (c) 2022 lim-kim930
+ */
+import { Options, AuthenticationType } from "nodemailer/lib/smtp-connection";
+import { Options as MailOptions } from "nodemailer/lib/mailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+interface Outbox extends Options {
+    alias?: string;
+    auth: AuthenticationType;
+}
+interface Inbox {
+    to: string | Array<string>;
+    cc?: string | Array<string>;
+}
+interface MailContent {
+    subject?: MailOptions["subject"];
+    html?: MailOptions["html"];
+    text?: MailOptions["text"];
+    to?: Inbox["to"];
+    cc?: Inbox["cc"];
+}
+export default class LimMailer {
+    private outbox;
+    private transporter;
+    private inbox;
+    constructor(outbox?: Outbox, inbox?: Inbox);
+    setOutbox(outbox: Outbox): void;
+    setInbox(inbox: Inbox): Inbox | undefined;
+    sendMail(mailContent: MailContent): Promise<Error | SMTPTransport.SentMessageInfo>;
+    get outboxAddress(): string | undefined;
+    get inboxAddress(): string | string[] | undefined;
+}
+export {};
